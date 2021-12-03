77de0d83d9716bb705125fea70bad11e42634cfd
annesh0 <67394141+annesh0@users.noreply.github.com>
2021-12-02 22:07:22 -0500

    Replaced user with category

--- src/app.py
+++ src/app.py
@@ -3,14 +3,14 @@ import os
 
 from db import db
 from flask import Flask
-from db import User
+from db import Category
 from db import Task
 
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "tasks.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -30,19 +30,19 @@ def failure_response(message, code=404):
 def default():
     return success_response("default")
 
-@app.route('/api/users/', methods=['GET'])
-def get_users():
+@app.route('/api/categories/', methods=['GET'])
+def get_categories():
     return success_response(
-        {u.serialize() for u in User.query.all()}
+        {c.serialize() for c in Category.query.all()}
     )
 
-@app.route('/api/user/<int:user_id>/', methods=['GET'])
-def get_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
-        return failure_response("user does not exist", 400)
+@app.route('/api/category/<int:category_id>/', methods=['GET'])
+def get_category(category_id):
+    category = Category.query.filter_by(id=category_id).first()
+    if not category:
+        return failure_response("category does not exist", 400)
 
-    return success_response(user.serialize(), 200)
+    return success_response(category.serialize(), 200)
 
 @app.route('/api/task/', methods=['POST'])
 def make_task():
@@ -52,30 +52,30 @@ def make_task():
     db.session.commit()
     return success_response(new_task.serialize(), 201)
 
-@app.route('/api/<int:user_id>/tasks/', methods=['POST'])
-def create_task(user_id):
+@app.route('/api/<int:category_id>/tasks/', methods=['POST'])
+def create_task(category_id):
     body= json.loads(request.data)
-    user = User.query.filter_by(id=user_id).first()
+    category = Category.query.filter_by(id=category_id).first()
     #new_task= Task(name=body.get('name'), description= body.get('description', priority = body.get('priority')))
     if not body.get("name") or not body.get("description") or not body.get("priority"):
         return failure_response("arguments not specified", 400)
-    if not user:
-        return failure_response("user not found", 404)
+    if not category:
+        return failure_response("Category not found", 404)
 
-    new_task = Task(name=body.get("name"), description=body.get("description"), priority = body.get("priority"), username_id=user_id)
+    new_task = Task(name=body.get("name"), description=body.get("description"), priority = body.get("priority"), categiry_id=category_id)
     db.session.add(new_task)
     db.session.commit()
     return success_response(new_task.serialize(), 201)
 
-@app.route("/api/users/", methods=["POST"])
-def create_user():
+@app.route("/api/category/", methods=["POST"])
+def create_category():
     body = json.loads(request.data)
-    if body is None or body.get("username") is None:
+    if body is None or body.get("category") is None:
         return failure_response("arguments not specified", 400)
-    new_user = User(username=body.get("username"))
-    db.session.add(new_user)
+    new_category = Category(category=body.get("category"))
+    db.session.add(new_category)
     db.session.commit()
-    return success_response(new_user.serialize(), 201)
+    return success_response(new_category.serialize(), 201)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- src/db.py
+++ src/db.py
@@ -4,20 +4,20 @@ db = SQLAlchemy()
 
 # your classes here
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Category(db.Model):
+    __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
     tasks = db.relationship("Task", order_by="Task.priority", cascade="delete")
 
     def __init__(self, **kwargs):
-        self.username = kwargs.get('username')
+        self.category = kwargs.get('category')
 
     def serialize(self):
 
         return {
             'id': self.id,
-            'username': self.username,
+            'category': self.category,
             'tasks': [t.serialize() for t in self.tasks]
         }
 
@@ -26,14 +26,14 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
-    priority = db.Column(db.Integer, nullable=False)
-    username_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    priority = db.Column(db.Boolean, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
         self.description = kwargs.get('description')
         self.priority = kwargs.get('priority')
-        self.username_id= kwargs.get('username_id')
+        self.category_id= kwargs.get('category_id')
 
     def serialize(self):
         return {
@@ -41,6 +41,5 @@ class Task(db.Model):
             'name': self.name,
             'description': self.description,
             'priority': self.priority,
-            'username_id': self.username_id
+            'category_id': self.category_id
         }
-
